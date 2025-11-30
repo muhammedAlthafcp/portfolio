01646d174874cd0d457537fd618d1204bf3eb6b5
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-11-30 21:14:38 +0530

    Fix App imports case sensitivity

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css'; // Tailwind or regular
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+// import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
